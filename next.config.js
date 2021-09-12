47f30cab5859a7dcbec16a5f1f98cd9f32715b66
Nick Brinser <nickbrinser@gmail.com>
2021-09-12 18:11:22 -0500

    Update tsConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+/**
+ * @type {import('next').NextConfig}
+ **/
+ const nextConfig = {
+  /* config options here */
 }
+
+module.exports = nextConfig
